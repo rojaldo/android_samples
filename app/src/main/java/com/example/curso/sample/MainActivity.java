78c9b816c36f235ebe0c6077532d87160932d4b9
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-04-24 11:23:02 +0200

    my calculator works

--- app/src/main/java/com/example/curso/sample/MainActivity.java
+++ app/src/main/java/com/example/curso/sample/MainActivity.java
@@ -13,6 +13,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int INIT = 0;
+    private final int FIRST_FIGURE = 1;
+    private final int SECOND_FIGURE = 2;
+    private final int RESULT = 3;
+
     private TextView textViewDisplay;
     private Button button2;
     private Button button1;
@@ -33,27 +38,107 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonPlus;
     private String display = "";
 
+    private int currentState = INIT;
+    private double firstFigure = 0;
+    private double secondFigure = 0;
+    private double result = 0;
+    private String operator = "";
+
+    void handleNumber(int myNumber){
+        switch (this.currentState){
+            case INIT:
+                this.firstFigure = myNumber;
+                this.display = "" + myNumber;
+                this.currentState = FIRST_FIGURE;
+                break;
+            case FIRST_FIGURE:
+                this.firstFigure = this.firstFigure * 10 + myNumber;
+                this.display = this.display + myNumber;
+                break;
+            case SECOND_FIGURE:
+                this.secondFigure = this.secondFigure * 10 + myNumber;
+                this.display = this.display + myNumber;
+                break;
+            case RESULT:
+                this.firstFigure = myNumber;
+                this.secondFigure = 0;
+                this.result = 0;
+                this.operator = "";
+                this.display = "" + myNumber;
+                this.currentState = FIRST_FIGURE;
+                break;
+        }
+
+    }
+
+    void handleSymbol(String mySymbol){
+        switch (this.currentState){
+            case INIT:
+                break;
+            case FIRST_FIGURE:
+                if(mySymbol == "+" || mySymbol == "-" || mySymbol == "x" || mySymbol == "/" ){
+                    this.operator = mySymbol;
+                    this.currentState = SECOND_FIGURE;
+                    this.display = this.display + mySymbol;
+                }
+                break;
+            case SECOND_FIGURE:
+                if(mySymbol == "=" ){
+                    this.resolve();
+                    this.display = this.display + mySymbol + this.result;
+                    this.currentState = RESULT;
+                }
+                break;
+            case RESULT:
+                if(mySymbol == "+" || mySymbol == "-" || mySymbol == "x" || mySymbol == "/" ) {
+                    this.firstFigure = this.result;
+                    this.secondFigure = 0;
+                    this.operator = mySymbol;
+                    this.display = this.result + this.operator;
+                    this.currentState = SECOND_FIGURE;
+                }
+                break;
+        }
+    }
+
+    private void resolve() {
+        switch (this.operator){
+            case "+":
+                this.result = this.firstFigure + this.secondFigure;
+                break;
+            case "-":
+                this.result = this.firstFigure - this.secondFigure;
+                break;
+            case "x":
+                this.result = this.firstFigure * this.secondFigure;
+                break;
+            case "/":
+                this.result = this.firstFigure / this.secondFigure;
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.textViewDisplay = (TextView) findViewById(R.id.textView);
-        this.button7 = (Button) findViewById(R.id.button_7);
-        this.button8 = (Button) findViewById(R.id.button_8);
-        this.button9 = (Button) findViewById(R.id.button_9);
-        this.buttonSlash = (Button) findViewById(R.id.button_slash);
-        this.button4 = (Button) findViewById(R.id.button_4);
-        this.button5 = (Button) findViewById(R.id.button_5);
-        this.button6 = (Button) findViewById(R.id.button_6);
-        this.buttonStar = (Button) findViewById(R.id.button_star);
-        this.button1 = (Button) findViewById(R.id.button_1);
-        this.button2 = (Button) findViewById(R.id.button_2);
-        this.button3 = (Button) findViewById(R.id.button_3);
-        this.buttonMinus = (Button) findViewById(R.id.button_minus);
-        this.buttonDot = (Button) findViewById(R.id.button_dot);
-        this.button0 = (Button) findViewById(R.id.button_0);
-        this.buttonEqual = (Button) findViewById(R.id.button_equal);
-        this.buttonPlus = (Button) findViewById(R.id.button_plus);
+        this.textViewDisplay = findViewById(R.id.textView);
+        this.button7 = findViewById(R.id.button_7);
+        this.button8 = findViewById(R.id.button_8);
+        this.button9 = findViewById(R.id.button_9);
+        this.buttonSlash = findViewById(R.id.button_slash);
+        this.button4 = findViewById(R.id.button_4);
+        this.button5 = findViewById(R.id.button_5);
+        this.button6 = findViewById(R.id.button_6);
+        this.buttonStar = findViewById(R.id.button_star);
+        this.button1 = findViewById(R.id.button_1);
+        this.button2 = findViewById(R.id.button_2);
+        this.button3 = findViewById(R.id.button_3);
+        this.buttonMinus = findViewById(R.id.button_minus);
+        this.buttonDot = findViewById(R.id.button_dot);
+        this.button0 = findViewById(R.id.button_0);
+        this.buttonEqual = findViewById(R.id.button_equal);
+        this.buttonPlus = findViewById(R.id.button_plus);
 
 
         myClickListener = new View.OnClickListener() {
@@ -61,67 +146,67 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 switch (v.getId()){
                     case R.id.button_1:
-                        display = display + "1";
+                        handleNumber(1);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_2:
-                        display = display + "2";
+                        handleNumber(2);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_3:
-                        display = display + "3";
+                        handleNumber(3);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_4:
-                        display = display + "4";
+                        handleNumber(4);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_5:
-                        display = display + "5";
+                        handleNumber(5);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_6:
-                        display = display + "6";
+                        handleNumber(6);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_7:
-                        display = display + "7";
+                        handleNumber(7);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_8:
-                        display = display + "8";
+                        handleNumber(8);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_9:
-                        display = display + "9";
+                        handleNumber(9);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_0:
-                        display = display + "0";
+                        handleNumber(0);
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_slash:
-                        display = display + "/";
+                        handleSymbol("/");
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_star:
-                        display = display + "x";
+                        handleSymbol("x");
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_minus:
-                        display = display + "-";
+                        handleSymbol("-");
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_plus:
-                        display = display + "+";
+                        handleSymbol("+");
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_dot:
-                        display = display + ".";
+                        handleSymbol(".");
                         textViewDisplay.setText(display);
                         break;
                     case R.id.button_equal:
-                        display = display + "=";
+                        handleSymbol("=");
                         textViewDisplay.setText(display);
                         break;
 
@@ -146,10 +231,10 @@ public class MainActivity extends AppCompatActivity {
         this.buttonDot.setOnClickListener(this.myClickListener);
         this.buttonEqual.setOnClickListener(this.myClickListener);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
